10e6e735ba386bd0253b82526fe820ec06d4d3c7
Vladimir Kozyr <kozyrdev@gmail.com>
2026-05-21 22:39:06 +0300

    fix: load nvm in ssh deploy script

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
   url: env("PUBLIC_URL", "https://api-simplefood.kozyr.info"),
   proxy: true,
 });
