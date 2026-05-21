8b888800b664f7120f4c50b96fdfba97eafd212e
Vladimir Kozyr <kozyrdev@gmail.com>
2026-05-21 10:39:39 +0300

    add github actions and fix cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,18 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://simplefood.kozyr.info',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
